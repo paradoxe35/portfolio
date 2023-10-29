77896df42104220500263bf121bcf38621e5028a
Paradoxe Ngwasi <35149259+paradoxe35@users.noreply.github.com>
2023-10-29 11:37:59 +0200

    minor fixes

--- src/features/project/entity.ts
+++ src/features/project/entity.ts
@@ -3,7 +3,7 @@ export class Project {
     public readonly id: string,
     public readonly title: string,
     public readonly content: string,
-    public readonly status: "draft" | "publish" | "archived",
+    public readonly status: "published" | "draft" | "archived",
     public readonly description: string,
     public readonly image: string,
     public readonly technology: string,
--- src/features/project/usecase.ts
+++ src/features/project/usecase.ts
@@ -2,7 +2,7 @@ import { ProjectRepository } from "./repository";
 
 export async function getProjects(repo: ProjectRepository) {
   const projects = await repo.getProjects();
-  return projects.filter((project) => project.status === "publish");
+  return projects.filter((project) => project.status === "published");
 }
 
 export function getProjectByID(repo: ProjectRepository, id: string) {
