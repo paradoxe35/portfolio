53c9d6fa9d0e7dfb67689007e333cd2ea1cf5227
Paradoxe Ngwasi <paradoxngwasi@gmail.com>
2023-11-01 06:59:50 +0000

    write docker configurations

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 push:
 	git remote | xargs -L1 git push --all
+
+docker-build:
+	docker build -t paradoxe-portfolio -f docker/Dockerfile .
--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  output: "export",
+  output:
+    process.env.NEXT_BUILD_OUTPUT_TYPE === "standalone"
+      ? "standalone"
+      : "export",
 };
 
 module.exports = nextConfig;
