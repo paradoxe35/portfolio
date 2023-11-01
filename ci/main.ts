9fd4efa99bd25179d1b2d6e01463b7e41a453d19
Paradoxe Ngwasi <paradoxngwasi@gmail.com>
2023-11-01 09:47:10 +0000

    update main.ts

--- ci/main.ts
+++ ci/main.ts
@@ -14,7 +14,7 @@ for (const key in consts) {
 }
 
 // Init values or Constants
-const TRIGGER_PLATFORMS_DEBOUNCE = 5 * 1000 * 50;
+const TRIGGER_PLATFORMS_DEBOUNCE = 3 * 1000 * 60;
 
 const firebase = initFirebaseInstance();
 const platforms: (() => void)[] = [gitlab];
