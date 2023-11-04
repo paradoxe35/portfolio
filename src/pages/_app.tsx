48305433df2916da8de40e99790337ba8ea59618
Paradoxe NGWASI <paradoxe.ngwasi@smatflow.com>
2023-11-04 21:52:30 +0000

    Update file _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import "@/ui/styles/globals.scss";
-import Script from "next/script";
+// import Script from "next/script";
 import type { AppProps /*, AppContext */ } from "next/app";
 import { FC, ReactElement, ReactNode, useEffect } from "react";
 import "aos/dist/aos.css";
@@ -27,9 +27,9 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
 
   return getLayout(
     <>
-      <Script id="sitespeak-script" type="text/javascript">
+      {/* <Script id="sitespeak-script" type="text/javascript">
         {`(function(){d=document;s=d.createElement("script");s.src="https://sitespeak.ai/chatbots/6e3433de-45e5-41e0-b6bb-a9cbfd2055e3.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();`}
-      </Script>
+      </Script> */}
       <Component {...pageProps} />
     </>
   );
